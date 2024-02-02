e3c1a8958f5bf17e5f4368ea044b8e9209073503
marouanej <157846319+marouanej@users.noreply.github.com>
2024-02-02 17:51:55 +0100

    Update settings.py

--- sell/settings.py
+++ sell/settings.py
@@ -32,7 +32,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
